7232c35b1601900653fb4c664be6b0014a7bef59
halukhndstn <145353400+halukhndstn@users.noreply.github.com>
2024-12-07 11:27:10 +0300

    map design

--- scripts/map_script.js
+++ scripts/map_script.js
@@ -1,22 +1,7 @@
-var map = L.map('map').setView([39.9334, 32.8597], 6);
+var map = L.map('map').setView([20, 0], 2); 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 19
 }).addTo(map);
-
-var locations = [
-    {lat: 38.676963089350345, lon: 26.750876194015795, popup: "Deneme"},
-];
-
-locations.forEach(function(location, index) {
-    var marker = L.marker([location.lat, location.lon]).addTo(map);
-    marker.bindPopup(location.popup);
-
-    var li = document.createElement('li');
-    li.textContent = location.popup.split('<strong>')[1].split('</strong>')[0];
-    li.onclick = function() {
-        map.setView([location.lat, location.lon], 10); 
-        marker.openPopup(); 
-    };
-    document.getElementById('locations').appendChild(li);
-});
